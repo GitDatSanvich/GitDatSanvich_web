3d9a53472b6ba56c0fa353ffbce90d3b3a58cfdf
tangchen <tc704321764@outlook.com>
2021-06-09 23:59:24 +0800

    提交代码

--- js/web_access.js
+++ js/web_access.js
@@ -1,6 +1,15 @@
 function web_access(login_check) {
     var sender = axios.create({});
     sender.get(base_url + "/access").then(result => {
-        console.log(result);
+        if(result.data.code===0){
+            if(!result.data.data.accessAble){
+                location.href="error.html";
+            }
+            var time = result.data.data.accessTime;
+            document.getElementById("access_time").innerHTML = time;
+        }else{
+            //失败直接跳转 error
+            location.href="error.html";
+        }
     });
 }
