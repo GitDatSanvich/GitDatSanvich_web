3393b71f090c6f9d80372802711362b28f95f89a
tangchen <tc704321764@outlook.com>
2021-06-16 10:03:27 +0800

    提交代码

--- js/base_url.js
+++ js/base_url.js
@@ -1,3 +1,3 @@
 // base_url="localhost:5000";
-// base_url="http://192.168.0.107:5500/api";
-base_url="https://www.gitdatsanvich.com/api";
+base_url="http://localhost:5500/api";
+// base_url="https://www.gitdatsanvich.com/api";
