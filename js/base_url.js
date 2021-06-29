b8ddec44413be47c4f4e3c3d43be6b40835d229a
tangchen <tc704321764@outlook.com>
2021-06-29 16:35:54 +0800

    提交代码

--- js/base_url.js
+++ js/base_url.js
@@ -1,2 +1,2 @@
-// base_url = "http://localhost:5500/api";
-base_url="https://www.gitdatsanvich.com/api";
+base_url = "http://localhost:5500/api";
+// base_url="https://www.gitdatsanvich.com/api";
--- js/web_access.js
+++ js/web_access.js
@@ -1,10 +1,10 @@
-function web_access(login_check) {
+function web_access(login_check, level) {
     var sender = axios.create({});
     sender.get(base_url + "/access").then(result => {
         console.log(result);
-        if(result.data.code===0){
-            if(!result.data.data.accessAble){
-                location.href="error.html";
+        if (result.data.code === 0) {
+            if (!result.data.data.accessAble) {
+                to_error(level);
             }
             var time = result.data.data.accessTime;
             var ip = result.data.data.ip;
@@ -12,9 +12,17 @@ function web_access(login_check) {
             document.getElementById("access_time").innerHTML = time;
             document.getElementById("access_id").innerHTML = ip;
             document.getElementById("access_time_total").innerHTML = access_total;
-        }else{
+        } else {
             //失败直接跳转 error
-            location.href="error.html";
+            to_error(level);
         }
     });
+}
+
+function to_error(level){
+    var outLevel = "";
+    for (i = 0; i < level; i++) {
+        outLevel = outLevel + "../";
+    }
+    location.href = outLevel + "error.html";
 }
